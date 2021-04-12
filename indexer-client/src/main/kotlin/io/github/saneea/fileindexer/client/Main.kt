585b1deb57dc54a9e848c1d1faaf31af001e966d
Alexander Sokol <saneea@bk.ru>
2021-04-13 00:00:37 +0300

    just remove unused imports

--- indexer-client/src/main/kotlin/io/github/saneea/fileindexer/client/Main.kt
+++ indexer-client/src/main/kotlin/io/github/saneea/fileindexer/client/Main.kt
@@ -1,9 +1,5 @@
 package io.github.saneea.fileindexer.client
 
-import io.github.saneea.fileindexer.core.service.FileIndexerService
-import io.github.saneea.fileindexer.core.tokenizer.Tokenizer
-import io.github.saneea.fileindexer.core.tokenizer.WhitespaceTokenizer
-import java.nio.file.Paths
 import javax.swing.JFrame
 
 import javax.swing.SwingUtilities
